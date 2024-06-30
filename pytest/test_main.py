860374b0682e848bdc8247e83475d49d87271caf
sh1ro9ma <seigyo8cpp1@yahoo.co.jp>
2024-06-30 12:32:53 +0900

    ファイルヘッダを書き出す

--- pytest/test_main.py
+++ pytest/test_main.py
@@ -18,16 +18,23 @@ def test_int2bytes_big_endian():
     assert main.int2bytes(255, 2, main.BIG_ENDIAN) == b'\x00\xff'
     assert main.int2bytes(305419896, 4, main.BIG_ENDIAN) == b'\x12\x34\x56\x78'
 
+
 def test_int2bytes_little_endian():
     assert main.int2bytes(1, 4, main.LITTLE_ENDIAN) == b'\x01\x00\x00\x00'
     assert main.int2bytes(255, 2, main.LITTLE_ENDIAN) == b'\xff\x00'
     assert main.int2bytes(305419896, 4, main.LITTLE_ENDIAN) == b'\x78\x56\x34\x12'
+
 
 def test_int2bytes_length():
     assert main.int2bytes(1, 1, main.LITTLE_ENDIAN) == b'\x01'
     assert main.int2bytes(1, 2, main.BIG_ENDIAN) == b'\x00\x01'
     assert main.int2bytes(65535, 2, main.BIG_ENDIAN) == b'\xff\xff'
     assert main.int2bytes(65535, 3, main.LITTLE_ENDIAN) == b'\xff\xff\x00'
+
+
+def test_make_file_header():
+    assert main.make_file_header() == b'\x42\x4D\x36\x10\x0E\x00\x00\x00\x00\x00\x36\x00\x00\x00'
+
 
 # pytestを使ったテストの実行
 if __name__ == "__main__":
--- src/main.py
+++ src/main.py
@@ -5,6 +5,16 @@ OUTPUT_FILE_PATH = "./src/output/" + OUTPUT_FILE_NAME
 LITTLE_ENDIAN = 0
 BIG_ENDIAN = 1
 
+FILE_WIDTH = 640
+FILIE_HEIGHT = 480
+PX_SIZE = 3
+IMAGE_SIZE = FILE_WIDTH * FILIE_HEIGHT * PX_SIZE
+
+FILE_HEADER_SIZE = 14
+INFO_HEADER_SIZE = 40
+FILE_SIZE = FILE_HEADER_SIZE + INFO_HEADER_SIZE + IMAGE_SIZE
+OFFSET = FILE_HEADER_SIZE + INFO_HEADER_SIZE
+
 # def int2bytes(int_val_r: int, length_r: int, endian: int) -> bytes:
 def int2bytes(int_val_r, length_r, endian):
     """int型をバイト列に変換する
@@ -37,6 +47,10 @@ def make_file_header():
     """
     bytes_data = b""
     bytes_data = bytes_data + int2bytes(0x424d, 2, BIG_ENDIAN)  # ファイルタイプ
+    bytes_data = bytes_data + int2bytes(FILE_SIZE, 4, LITTLE_ENDIAN)  # ファイルサイズ
+    bytes_data = bytes_data + int2bytes(0, 2, LITTLE_ENDIAN)  # 予約領域
+    bytes_data = bytes_data + int2bytes(0, 2, LITTLE_ENDIAN)  # 予約領域
+    bytes_data = bytes_data + int2bytes(OFFSET, 4, LITTLE_ENDIAN)  # "ファイル先頭から"画像データまでのオフセット  ☆
     return bytes_data
 
 
@@ -44,7 +58,9 @@ def main():
     bytes_data = b""
     bytes_data = bytes_data + make_file_header()
 
-    print(bytes_data)
+
+    with open(OUTPUT_FILE_PATH, "wb") as f:
+        f.write(bytes_data)
 
 
 if __name__ == "__main__":
